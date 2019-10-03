6ba63a98f96884243c888be4892eec5ae64b02f1
ganeshmastud <47074753+ganeshmastud@users.noreply.github.com>
2019-10-04 00:30:16 +0530

    Images and server

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ app.use(cors());
 
 var register = require('./routes/register');
 var login = require('./routes/login');
+var upload_img = require('./upload_img');
 
 app.use('/register',register);
 app.use('/login',login);
+app.use('/upload_img',upload_img);
 
 app.listen(4000,(req,res)=>{
     console.log("Server started!!!")
